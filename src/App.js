942b88946f858f8f7e0eefa0136aaf79177a0eb3
Jamie Haywood <jamie.haywood@gmail.com>
2019-03-12 12:09:53 +0000

    Updated App.js boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import TubeIcon from './components/TubeIcon';
+import InputBox from './components/InputBox';
+import SubmitButton from './components/SubmitButton';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="title">
+          Which Side?
+        </div>
+        
+        <InputBox placeholder="Station One"/>
+        <InputBox placeholder="Station Two"/>
+        <SubmitButton text="Check!"/>
+        <TubeIcon leftSide="#ffffff" rightSide="#ffffff" />
       </div>
     );
   }
