b52e6477ddf3272013743eff0daa23ac96ffaeed
Jamie Haywood <jamie.haywood@gmail.com>
2019-03-19 17:38:44 +0000

    Updates

---
+++ precache-manifest.2f9221f36517c5bd15c9a1e1fe68262e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c9e6f5a84f1e9421626d4bb96d0314d",
+    "url": "/WhichSide/static/media/Johnston100W03-Medium.9c9e6f5a.woff2"
+  },
+  {
+    "revision": "997ae1e9c678af7608b0",
+    "url": "/WhichSide/static/css/main.ef46d6cd.chunk.css"
+  },
+  {
+    "revision": "0bb65295db472f0e1b20",
+    "url": "/WhichSide/static/js/runtime~main.7e554950.js"
+  },
+  {
+    "revision": "2a31aa072b8f3f84c671",
+    "url": "/WhichSide/static/js/2.e4b2b633.chunk.js"
+  },
+  {
+    "revision": "ef0b8e643bac5abcde656f93ab323c53",
+    "url": "/WhichSide/static/media/Johnston100W03-Light.ef0b8e64.eot"
+  },
+  {
+    "revision": "b0b5e0e86f9abb4a791d69e673fffe88",
+    "url": "/WhichSide/static/media/Johnston100W03-Light.b0b5e0e8.woff2"
+  },
+  {
+    "revision": "adf5cbb89800ccf949a7db31013b3cbe",
+    "url": "/WhichSide/static/media/Johnston100W03-Regular.adf5cbb8.eot"
+  },
+  {
+    "revision": "fd0a76c7579d9c22fe1d6077a6a8bb87",
+    "url": "/WhichSide/static/media/Johnston100W03-Regular.fd0a76c7.woff2"
+  },
+  {
+    "revision": "f5ee8c89414a2ae191bf981b3ee6e12c",
+    "url": "/WhichSide/static/media/Johnston100W03-Medium.f5ee8c89.eot"
+  },
+  {
+    "revision": "997ae1e9c678af7608b0",
+    "url": "/WhichSide/static/js/main.fbd0290c.chunk.js"
+  },
+  {
+    "revision": "be499e7120ddfa59344370a90a380876",
+    "url": "/WhichSide/static/media/Johnston100W03-Light.be499e71.woff"
+  },
+  {
+    "revision": "08cbc8eb9051799bad57a836ca2135d5",
+    "url": "/WhichSide/static/media/Johnston100W03-Regular.08cbc8eb.woff"
+  },
+  {
+    "revision": "a7afe23276a8b4311238980510687c25",
+    "url": "/WhichSide/static/media/Johnston100W03-Medium.a7afe232.woff"
+  },
+  {
+    "revision": "dbb9726f467f87d1acf343ce98704569",
+    "url": "/WhichSide/static/media/Johnston100W03-Light.dbb9726f.ttf"
+  },
+  {
+    "revision": "0ea5a2ceffb9af477576260aeac4d3ed",
+    "url": "/WhichSide/static/media/Johnston100W03-Regular.0ea5a2ce.ttf"
+  },
+  {
+    "revision": "4c6e937d68278d9ac76c72921cfdf8b8",
+    "url": "/WhichSide/static/media/Johnston100W03-Medium.4c6e937d.ttf"
+  },
+  {
+    "revision": "18caf42645db45904fc711838d60c583",
+    "url": "/WhichSide/static/media/Johnston100W03-Medium.18caf426.svg"
+  },
+  {
+    "revision": "3e4884c3f76fd92a82191cf912d10a06",
+    "url": "/WhichSide/static/media/Johnston100W03-Regular.3e4884c3.svg"
+  },
+  {
+    "revision": "62394052e92a0795ca66440cd01f631a",
+    "url": "/WhichSide/index.html"
+  }
+];
