45beb0478ca51aab2cc90e90a9c09081c4188c15
Jamie Haywood <jamie.haywood@gmail.com>
2019-04-29 14:57:09 +0100

    changed location of whichSide logic

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TubeIcon from './components/TubeIcon';
 import InputBox from './components/InputBox';
 import SubmitButton from './components/SubmitButton';
 // import Roundel from './components/Roundel';
-import {whichSide} from './data/data.js'
+import {whichSide} from './logic/whichSide.js'
 
 class App extends Component {
   constructor() {
