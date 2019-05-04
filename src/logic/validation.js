d4abd2693f47c0cd0e6ea8b9823ae732307b25b3
Jamie Haywood <jamie.haywood@gmail.com>
2019-05-04 21:16:45 +0100

    created a hacky little callback variable to enable nested true or false returns

--- src/logic/validation.js
+++ src/logic/validation.js
@@ -1,15 +1,19 @@
 import { data } from '../data/data'
 
 export function validate(originStation, destinationStation, inputLine) {
+    var callback;
 
-    // Checking if input is not null or empty using truthy
+    isNotEmptyOrNullCheck(originStation, destinationStation, inputLine)
+
+    function isNotEmptyOrNullCheck(originStation, destinationStation, inputLine){
     if (originStation && destinationStation && inputLine) {
         stationNameCheck(data)
     }
     else {
         alert("Please fill in all the fields")
-        return false;
+        }
     }
+
 
     function stationNameCheck(data) {
         let stationNames = data.map(a => a.stationName)
@@ -18,11 +22,11 @@ export function validate(originStation, destinationStation, inputLine) {
         if (stationNames.includes(originStation) &&
             stationNames.includes(destinationStation) &&
             lineNames.includes(inputLine)) {
-            return true
+            callback = true;
             }
         else {
             alert("Please check your station and line names")
-            return false
         }
     }
+    return callback
 }
